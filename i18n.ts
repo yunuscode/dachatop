60a656e191eb53fc773b470ee16d44f62d5a11cd
yunuscode <yunuzmuhammad1@gmail.com>
2023-07-31 13:42:00 +0500

    FEAT: add i18n module

---
+++ i18n.ts
@@ -0,0 +1,58 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import { locale } from "expo-localization";
+
+const resources = {
+  en: {
+    translation: {
+      "Welcome to React": "Welcome to React and react-i18next",
+      "Select language": "Select language",
+      "Login with phone number": "Login with phone number",
+      "Please enter your phone number": "Please enter your phone number",
+      "Phone number": "Phone number",
+      "Login": "Login",
+      "Confirm OTP code": "Confirm OTP code",
+      "We've sent OTP code to your phone number. Please enter it.": "We've sent OTP code to your phone number. Please enter it.",
+      "Confirm": "Confirm",
+    },
+  },
+  ru: {
+    translation: {
+      "Welcome to React": "Bienvenue à React et react-i18next",
+      "Select language": "Выберите язык",
+      "Login with phone number": "Войти с номером телефона",
+      "Please enter your phone number": "Пожалуйста введите ваш номер телефона",
+      "Phone number": "Phone number",
+      "Login": "Login",
+      "Confirm OTP code": "Confirm OTP code",
+      "We've sent OTP code to your phone number. Please enter it.": "We've sent OTP code to your phone number. Please enter it."
+    },
+  },
+  uz: {
+    translation: {
+      "Welcome to React": "Bienvenue à React et react-i18next",
+      "Select language": "Tilni tanlang",
+      "Login with phone number": "Telefon raqam orqali kirish",
+      "Please enter your phone number":
+        "Iltimos, telefon raqamingizni to'g'ri kiriting!",
+      "Phone number": "Telefon raqamingiz",
+      "Login": "Kirish",
+      "Confirm OTP code": "SMS kodni tasdiqlash",
+      "We've sent OTP code to your phone number. Please enter it.": "Biz sizga sms orqali kod yubordik, iltimos shuni kiriting!",
+      "Confirm": "Tasdiqlash",
+      "OTP code": "SMS kod"
+    },
+  },
+};
+
+i18n.use(initReactI18next).init({
+  compatibilityJSON: "v3",
+  resources,
+  lng: locale,
+  fallbackLng: "en",
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+export default i18n;
