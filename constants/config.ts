f1a48f5ad38e6155a0310348365ee2127984fa62
mmirnosir <129169137+mmirnosir@users.noreply.github.com>
2025-06-14 12:29:25 +0100

    Update config.ts

--- constants/config.ts
+++ constants/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = "https://dacha.pixer.uz/v1/";
-export const SERVER_URL = "https://dacha.pixer.uz/";
+export const API_URL = "https://dacha-back-production.up.railway.app/v1/";
+export const SERVER_URL = "https://dacha-back-production.up.railway.app/";
 // export const API_URL = "http://localhost:3000/v1/";
 // export const SERVER_URL = "http://localhost:3000/";
