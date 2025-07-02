2b4e67f2c6a05fc0d5ee2fc5a7d945ff165f5980
mmirnosir <129169137+mmirnosir@users.noreply.github.com>
2025-07-02 12:38:45 +0100

    Update config.ts

--- constants/config.ts
+++ constants/config.ts
@@ -1,4 +1,4 @@
 export const API_URL = "https://dacha-back-production.up.railway.app/v1/";
-export const SERVER_URL = "https://dacha-back-production.up.railway.app/";
+export const SERVER_URL = "https://bucket-production-de56.up.railway.app/";
 // export const API_URL = "http://localhost:3000/v1/";
 // export const SERVER_URL = "http://localhost:3000/";
